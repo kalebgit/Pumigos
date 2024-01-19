06d759a6f821347cc5e871a1aaf6abf4f46c653f
Emiliano Jimenez <emilianokaleb@gmail.com>
2024-01-18 18:05:49 -0600

    chore:

---
+++ client/src/pages/Post.jsx
@@ -0,0 +1,10 @@
+import FormContainer from "../components/Forms/FormContainer";
+
+
+export default function Post(){
+    return(
+        <FormContainer>
+            <Input/>
+        </FormContainer>
+    )
+}
